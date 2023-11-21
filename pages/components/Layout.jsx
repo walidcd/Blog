30902d8bf914dd3d5a02287c5c3ca8cbe0c79517
BOUTAHAR <walidboutahar@Walids-MacBook-Air.local>
2023-11-21 14:58:39 +0100

    burger menu

--- pages/components/Layout.jsx
+++ pages/components/Layout.jsx
@@ -4,7 +4,7 @@ import Navbar from "./Navbar";
 const Layout = ({children}) => {
     return (
         <div className="w-full h-full dark:text-white dark:bg-gray-900 overflow-y-scroll">
-        <div className="max-w-5xl mx-auto px-7  w-full pb-auto flex flex-col h-full">
+        <div className="max-w-4xl mx-auto px-7  w-full pb-auto flex flex-col h-full">
             <Navbar/>
                 {children}
             <Footer/>
