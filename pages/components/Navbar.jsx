14933338e40d17e6a2bd1f57b694020d6290fe96
BOUTAHAR <walidboutahar@Walids-MacBook-Air.local>
2023-11-21 16:00:18 +0100

    burger menu

--- pages/components/Navbar.jsx
+++ pages/components/Navbar.jsx
@@ -7,6 +7,9 @@ const Navbar = () => {
     const toggleMenu = () => {
         setShowMenu(!showMenu);
     };
+    const hideMenu = () => {
+        setShowMenu(false);
+    };
     return ( 
         <nav className="bg-white border-gray-200 dark:bg-gray-900">
             <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
@@ -22,51 +25,26 @@ const Navbar = () => {
                 >
                     <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
-                    </svg>                </button>
+                    </svg>                
+                </button>
                 <div className={`md:flex w-full md:w-auto md:order-1 ${showMenu ? 'block' : 'hidden'}`} id="navbar-language">
                         <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
                             <li>
-                                <Link href="/" className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500" aria-current="page">Home</Link>
+                                <Link href="/" className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500" aria-current="page" onClick={hideMenu}>Home</Link>
                             </li>
                             <li>
-                                <Link href="/About" className="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">About</Link>
+                                <Link href="/About" className="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700" onClick={hideMenu}>About</Link>
                             </li>
                             {/* <li>
                                 <Link href="#" className="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Blogs</Link>
                             </li> */}
                             <li>
-                                <Link href="/Books" className="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Books</Link>
+                                <Link href="/Books" className="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700" onClick={hideMenu}>Books</Link>
                             </li>
                     </ul>
                 </div>
             </div>
         </nav>
-        // <nav className="bg-white border-gray-200 dark:bg-gray-900">
-        // <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-        // <Link href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
-        //     <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">{"<"}</span>
-        //     <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">{"WALID"}</span>
-        //     <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">{"/>"}</span>
-        // </Link>
-        // <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-language">
-        //     <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-        //     <li>
-        //         <Link href="/" className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500" aria-current="page">Home</Link>
-        //     </li>
-        //     <li>
-        //         <Link href="/About" className="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">About</Link>
-        //     </li>
-        //     {/* <li>
-        //         <Link href="#" className="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Blogs</Link>
-        //     </li> */}
-        //     <li>
-        //         <Link href="/Books" className="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Books</Link>
-        //     </li>
-        //     </ul>
-        // </div>
-        // </div>
-        // </nav>
-
      );
 }
  
